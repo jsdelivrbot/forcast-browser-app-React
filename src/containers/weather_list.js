2fa1dba406036bd10a0bb07ee2939389f7d6bb6d
roaringrussian <roaringrussian@aol.com>
2017-09-29 12:20:40 -0600

    fixed all errors, code is working and shows the data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,47 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart  from '../components/chart';
 
 class WeatherList extends Component {
-  renderWeather(cityData) {
-    const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
-
-
-    return (
-      <tr key={ name }>
-        <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red"></SparklinesLine>
-          </Sparklines>
-        </td>
-      </tr>
-    )
-  }
-
-
-  render(){
-    return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
-    );
-  }
+    renderWeather(cityData) {
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        return (
+            <tr key={cityData.city.id} >
+                <td>{cityData.city.name}</td>
+                <td><Chart data={temps} color="blue" /></td>
+                <td><Chart data={humidities} color="green" /></td>
+                <td><Chart data={pressures} color="red" /></td>
+            </tr>
+        );
+    }
+    render() {
+        //this.renderWeather = this.renderWeather.bind(this)
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    };
 }
-
-function mapStateToProps({ weather }) {//same as doing const weather = state.weather.
-  return { weather }; //ES6 {weather } === { weather: weather}
+function mapStateToProps({ weather }) {//Pulls weather from props.weather
+  //same as doing const weather = state.weather.
+    return { weather }; //ES6 {weather } === { weather: weather}
 }
-
 export default connect(mapStateToProps)(WeatherList);
