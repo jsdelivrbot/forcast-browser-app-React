1c62553dbe22db7d68b25bf4554bc90e494f1aad
roaringrussian <roaringrussian@aol.com>
2017-10-02 11:18:38 -0600

    added lng and lat for googlemaps property

--- src/components/google_maps.js
+++ src/components/google_maps.js
@@ -2,8 +2,18 @@ import React, {Componet } from "react";
 
 
 class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.long
+      }
+    });
+  }
+
   render() {
-    return <div ref="map" />
+    return <div ref="map" />;//use this.refs.map will refer to the html element here.
   }
 }
 
