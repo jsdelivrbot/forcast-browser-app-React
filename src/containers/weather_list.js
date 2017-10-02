1587f99fea84493e14fd497bfcea4e5d24c6f704
roaringrussian <roaringrussian@aol.com>
2017-10-02 11:32:33 -0600

    added googlemaps data to the react side of the app

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart  from '../components/chart';
+import GoogleMap from '..components/google_maps';
 
 class WeatherList extends Component {
-  
+
     renderWeather(cityData) {
         const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (9/5 * (temp - 273) + 32));
         const humidities = cityData.list.map(weather => weather.main.humidity);
         const pressures = cityData.list.map(weather => weather.main.pressure);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={cityData.city.id} >
-                <td>{cityData.city.name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="blue" units="F" /></td>
                 <td><Chart data={humidities} color="green" units="hPa" /></td>
                 <td><Chart data={pressures} color="red" units="%"/></td>
