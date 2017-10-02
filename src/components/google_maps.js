81507db07a0e2dbdfbb217137ee4fe02b64ca25c
roaringrussian <roaringrussian@aol.com>
2017-10-02 11:48:15 -0600

    fixed the google maps error

--- src/components/google_maps.js
+++ src/components/google_maps.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 
 
 class GoogleMap extends Component {
-  componentDidMount() {
+  componentDidMount(){
     new google.maps.Map(this.refs.map, {
-      zoom: 12,
-      center: {
-        lat: this.props.lat,
-        lng: this.props.long
-      }
+        zoom : 12,
+        center : new google.maps.LatLng(this.props.lat, this.props.lon)
     });
-  }
+}
 
   render() {
     return <div ref="map" />;//use this.refs.map will refer to the html element here.
